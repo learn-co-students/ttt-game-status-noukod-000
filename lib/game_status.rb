66e4fcb1ea6beeb2ef2617e987b412f2cc63e334
Veniel Dorestave <veniel@noukod.com>
2018-09-03 19:41:01 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,54 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def won?(board)
+  WIN_COMBINATIONS.find do |combo|
+    # original argument "position_taken?(board, combo)"
+    position_taken?(board, combo[0]) &&
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[0]] == board[combo[2]]
+  end
+end
+def full?(board)
+  board.all? do |index|
+    index != " "
+  end
+end
+
+def draw?(board)
+  full?(board) && won?(board) == nil
+end
+
+
+def over?(board)
+if  draw?(board) || won?(board)
+  full?(board)
+  return true
+else
+  return false
+end
+end
+
+def winner(board)
+  if won?(board)
+   return board[won?(board)[0]]
+  end
+end
