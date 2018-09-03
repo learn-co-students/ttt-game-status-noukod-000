b6717b557580cc926acfc97b16eaa5e31345bc8c
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-03 20:32:34 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,27 +26,69 @@ end
 
 # Define won?, full?, draw?, over?, and winner below
 def won?(board)
-  WIN_COMBINATIONS.detect do |pos|
-    board[pos[0]] == board[pos[1]] &&
-    board[pos[1]] == board[pos[2]] &&
-    position_taken?(board, pos[0])
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    if ((position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O"))
+      return win_combination
+    else
+      # do nothing
+    end
   end
+  return nil
 end
 
 def full?(full_board)
-  full_board.all?{|token| token == "X" || token == "O"}
+  counter = 0
+  full_board.each do |element|
+    if (element == "X" || element == "O")
+      counter += 1
+    else
+      # do nothing
+    end
+  end
+  if counter == 9
+    return true
+  else
+    return false
+  end
 end
 
 def draw?(board)
-  full?(board) && !won?(board)
+  if full?(board) && !won?(board)
+    return true
+  else
+    return false
+  end
 end
 
 def over?(board)
-  won?(board) || full?(board)
+  if ( won?(board) || draw?(board) || full?(board) )
+    return true
+  else
+    return false
+  end
 end
 
 def winner(board)
-  if win_pos = won?(board)
-    board[win_pos.first]
+  if won?(board)
+    WIN_COMBINATIONS.each do |win_combination|
+      position_1 = board[win_combination[0]]
+      position_2 = board[win_combination[1]]
+      position_3 = board[win_combination[2]]
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X")
+        return "X"
+      elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return "O"
+      else
+        # do nothing
+      end
+    end
+  else
+    return nil
   end
 end
