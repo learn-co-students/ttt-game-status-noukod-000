22d6f8f9b031cac5a147ad52653857a54743649a
Jerry Charles <jerrycharles586@gmail.com>
2019-04-09 00:12:08 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,3 +1,4 @@
+# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
@@ -15,62 +16,6 @@ WIN_COMBINATIONS = [
   [2,5,8]
   ]
 board = ["X", "X", "O", "X", "O", "X", "X", "X", "O"]
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(user_input)
-  index = user_input.to_i - 1
-end
-
-def move(board, index, current_player)
-  board[index] = current_player
-end
-
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def turn_count(board)
-  turns = 0
-  board.each do |le|
-    if le == "X" || le == "O"
-      turns +=1 
-    end
-  end
-  return turns
-end
-
-def current_player(board)
-  var = turn_count(board)
-  if var % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index, current_player(board))
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
 
 def won?(board)
   winner = nil
@@ -127,14 +72,3 @@ def winner(board)
   winner
 end
 winner(board)
-
-def play(board)
-  while !over?(board)
-    turn(board)
-  end
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
-    puts "Cat's Game!"
-  end
-end
